0c30288dc464ce0104daf4c1012bb59e3f9c9166
doc jones <95450533+butterscotchpancake@users.noreply.github.com>
2024-08-13 19:06:53 -0500

    Update remove_old_users.sh

--- remove_old_users.sh
+++ remove_old_users.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # shut down all ezarr containers referenced by docker-compose.yml 
-# If you previously had ezarr installed somewhere else you have to do this manually in the directory where the docker-compose file is.
 sudo docker compose down
 
 # Remove old users and group
@@ -9,15 +8,10 @@ sudo userdel sonarr
 sudo userdel radarr
 sudo userdel lidarr
 sudo userdel readarr
-sudo userdel mylar
-sudo userdel audiobookshelf
 sudo userdel bazarr
 sudo userdel prowlarr
 sudo userdel jackett
-sudo userdel plex
-sudo userdel overseerr
 sudo userdel jellyseerr
 sudo userdel qbittorrent
-sudo userdel sabnzbd
+sudo userdel whisparr
 sudo groupdel mediacenter
-
